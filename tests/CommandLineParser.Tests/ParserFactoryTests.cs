78eaf7a169480b6a86e6c8e06113482176452d1d
dboikliev <deyanpb@hotmail.com>
2016-11-14 21:49:11 +0200

    Adds unit tests for the ParserFactory.

--- tests/CommandLineParser.Tests/ParserFactoryTests.cs
+++ tests/CommandLineParser.Tests/ParserFactoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using CommandLineParser.Exceptions;
 using CommandLineParser.TypeParsers;
 using Xunit;
@@ -8,15 +9,39 @@ namespace CommandLineParser.Tests
     public class ParserFactoryTests
     {
         [Fact]
-        public void GetParserInt32ReturnsInt32Parser() 
+        public void GetInt32Parser() 
         {
             var factory = new ParserFactory();
-            var int32Parser = factory.GetParser(typeof(Int32));
+            var int32Parser = factory.GetParser(typeof(int));
             Assert.IsType<Int32Parser>(int32Parser);
         }
 
         [Fact]
-        public void GetParserObjectThrowsTypeNotSupportedException()
+        public void GetStringParser()
+        {
+            var factory = new ParserFactory();
+            var enumberableInt32Parser = factory.GetParser(typeof(string));
+            Assert.IsType<StringParser>(enumberableInt32Parser);
+        }
+
+        [Fact]
+        public void GetIEnumberableInt32Parser()
+        {
+            var factory = new ParserFactory();
+            var enumberableInt32Parser = factory.GetParser(typeof(IEnumerable<int>));
+            Assert.IsType<EnumerableValueParser<int>>(enumberableInt32Parser);
+        }
+
+        [Fact]
+        public void GetIEnumberableStringParser()
+        {
+            var factory = new ParserFactory();
+            var enumberableInt32Parser = factory.GetParser(typeof(IEnumerable<string>));
+            Assert.IsType<EnumerableValueParser<string>>(enumberableInt32Parser);
+        }
+
+        [Fact]
+        public void GetObjectParserThrowsTypeNotSupportedException()
         {
             var factory = new ParserFactory();
             Assert.Throws<TypeNotSupportedException>(() => factory.GetParser(typeof(object)));
